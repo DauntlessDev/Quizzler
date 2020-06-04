914ddd1c4aee1ed3448234b6d672a7422c32b498
DauntlessDev <braveleuterio.dauntless@gmail.com>
2020-06-04 15:47:09 +0800

    fixed score error

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  int score;
+  int score = 0;
   final quizBrain = QuizBrain();
 
   void addScore(bool answerState) {
